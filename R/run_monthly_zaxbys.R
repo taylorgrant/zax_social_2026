07bb95a1fbc3e96a842d67d44a692fa5cc4203e4
taylorgrant <taylorgrant38@gmail.com>
2026-04-18 13:50:50 -0700

    wrapper function to run

---
+++ R/run_monthly_zaxbys.R
@@ -0,0 +1,9 @@
+# Load libraries
+pacman::p_load(tidyverse, janitor, here, glue, googlesheets4, googledrive)
+
+# load all functions
+source(here::here("R", "source_project_files.R"))
+source_project_files()
+
+# run with specific month
+monthly_zaxbys("feb26")
