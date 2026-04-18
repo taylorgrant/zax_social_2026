12c91206cc123e8e9cd59237dd460030ef5853f9
taylorgrant <taylorgrant38@gmail.com>
2026-04-18 13:50:33 -0700

    load functions

---
+++ R/source_project_files.R
@@ -0,0 +1,9 @@
+source_project_files <- function() {
+  source(here::here("R", "config_helpers.R"))
+  source(here::here("R", "file_helpers.R"))
+  source(here::here("R", "read_helpers.R"))
+  source(here::here("R", "summary_helpers.R"))
+  source(here::here("R", "plot_helpers.R"))
+  source(here::here("R", "write_helpers.R"))
+  source(here::here("R", "monthly_zaxbys.R"))
+}
